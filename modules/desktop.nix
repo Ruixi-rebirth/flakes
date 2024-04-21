f7efe9c8e28c4988167448e02d1e0c90402d8c3d
Ruixi-rebirth <ruixirebirth@gmail.com>
2024-04-21 20:53:09 +0800

    fcitx5: enable waylandFrontend
    fcitx5: enable waylandFrontend

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -22,7 +22,10 @@
 
   i18n.inputMethod = {
     enabled = "fcitx5";
-    fcitx5.addons = with pkgs; [ fcitx5-rime fcitx5-chinese-addons fcitx5-table-extra fcitx5-pinyin-moegirl fcitx5-pinyin-zhwiki ];
+    fcitx5 = {
+      waylandFrontend = true;
+      addons = with pkgs; [ fcitx5-rime fcitx5-chinese-addons fcitx5-table-extra fcitx5-pinyin-moegirl fcitx5-pinyin-zhwiki ];
+    };
   };
 
   environment = {
