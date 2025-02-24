a3a33866af4d6f42a6c1bd0fa75b2055c42b0c73
Ruixi-rebirth <ruixirebirth@gmail.com>
2025-02-25 01:28:12 +0800

    init

---
+++ home/dev/rust.nix
@@ -0,0 +1,35 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  myRustModule =
+    with lib;
+    let
+      cfg = config.programs.rust;
+    in
+    {
+      options.programs.rust = {
+        enable = mkEnableOption "Rust";
+
+        package = mkOption {
+          type = types.package;
+          default = pkgs.rust-bin.stable.latest.default;
+          description = "The Rust package to use.";
+        };
+      };
+
+      config = mkIf cfg.enable {
+        home.packages = [ cfg.package ];
+      };
+    };
+in
+{
+  imports = [ myRustModule ];
+
+  programs.rust = {
+    enable = true;
+  };
+}
---
+++ hosts/yu/default.nix
@@ -0,0 +1,26 @@
+{
+  pkgs,
+  me,
+  lib,
+  ...
+}:
+{
+  networking.hostName = "yu";
+
+  wsl = {
+    enable = true;
+    defaultUser = "${me.userName}";
+    useWindowsDriver = true;
+    nativeSystemd = true;
+  };
+
+  environment = {
+    systemPackages = with pkgs; [
+      clipboard-jh
+      fastfetch
+    ];
+  };
+
+  system.stateVersion = "23.11";
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+}
