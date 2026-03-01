7be6d37cb3e188dfdc6e4fdac3792c76daa6abf2
Ruixi-rebirth <ruixirebirth@gmail.com>
2026-03-01 22:54:28 +0800

    feat(fish): add fzf-fish plugin

--- home/shell/fish/default.nix
+++ home/shell/fish/default.nix
@@ -1,4 +1,4 @@
-{ config, ... }:
+{ config, pkgs, ... }:
 {
   programs.fish = {
     enable = true;
@@ -23,12 +23,17 @@
       nf = ''nvim (FZF_DEFAULT_COMMAND='fd' FZF_DEFAULT_OPTS="--preview 'bat --style=numbers --color=always --line-range :500 {}'" fzf --height 60% --layout reverse --info inline --border --color 'border:#b48ead')'';
       top = "btop";
     };
-
     functions = {
       f = ''
         FZF_DEFAULT_COMMAND='fd --type f --hidden --follow --exclude .git' FZF_DEFAULT_OPTS="--color=bg+:#4C566A,bg:#424A5B,spinner:#F8BD96,hl:#F28FAD  --color=fg:#D9E0EE,header:#F28FAD,info:#DDB6F2,pointer:#F8BD96  --color=marker:#F8BD96,fg+:#F2CDCD,prompt:#DDB6F2,hl+:#F28FAD --preview 'bat --style=numbers --color=always --line-range :500 {}'" fzf --height 60% --layout reverse --info inline --border --color 'border:#b48ead'
       '';
     };
+    plugins = [
+      {
+        name = "fzf-fish";
+        src = pkgs.fishPlugins.fzf-fish;
+      }
+    ];
   };
   home.file = {
     ".config/fish/conf.d/nord.fish".text = import ./nord_theme.nix;
