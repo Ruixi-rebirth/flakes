a7ade94289e8c0ee00629f73c966c1a708d7e9fd
Ruixi-rebirth <ruixirebirth@gmail.com>
2026-03-07 15:36:27 +0800

    chore(fish): remove fzf-fish plugin

--- home/shell/fish/default.nix
+++ home/shell/fish/default.nix
@@ -28,12 +28,7 @@
         FZF_DEFAULT_COMMAND='fd --type f --hidden --follow --exclude .git' FZF_DEFAULT_OPTS="--color=bg+:#4C566A,bg:#424A5B,spinner:#F8BD96,hl:#F28FAD  --color=fg:#D9E0EE,header:#F28FAD,info:#DDB6F2,pointer:#F8BD96  --color=marker:#F8BD96,fg+:#F2CDCD,prompt:#DDB6F2,hl+:#F28FAD --preview 'bat --style=numbers --color=always --line-range :500 {}'" fzf --height 60% --layout reverse --info inline --border --color 'border:#b48ead'
       '';
     };
-    plugins = [
-      {
-        name = "fzf-fish";
-        src = pkgs.fishPlugins.fzf-fish;
-      }
-    ];
+    plugins = [];
   };
   home.file = {
     ".config/fish/conf.d/nord.fish".text = import ./nord_theme.nix;
